15d2a721ca63bb48127f6549d1de4fbfb7de2caf
David Hoppe <dave@hopasaurus.com>
2017-06-25 20:58:09 -0400

    Added Comments capturing the rules to be implemented.
    Came accross an interesting issue and made notes of it.
    
    If deciding to use mocks for testing is something that almost seems
    to be the default. Sometimes people need to think harder about it.
    
    In this case I decide to try to avoid mock as we can proceed without
    needing it.  Will be interesting to see if this comes back to haunt me.

--- cs/ConwayTest/RulesTest.cs
+++ cs/ConwayTest/RulesTest.cs
@@ -11,9 +11,24 @@ namespace ConwayTest
     [TestFixture]
     public class RulesTest
     {
+        // Rules are:
+        // Live cell with less than two neighbors dies
+        // Live cell with two or three neighbors lives
+        // Live cell with more than three neighbors dies
+        // Dead cell with three neighbors comes to life
+
         [Test]
-        public void TestRules()
+        public void Test_LiveCell_ZeroNeighbors_Dies()
         {
+            // pausing to think here....
+            // should we use mocking to answer the neighbor count?
+            // that seems heavy handed. I think for this case
+            // make a setup method that we can give a neighbor count to
+            // and have it set the cell up with the correct number of living neighbors.
+            // If counting neighbors on the cell were to cause disk/network access or
+            // were computationally intense the mocks would be more likely to be used.
+
+            var cell = new Cell();
             Rules.Foo();
         }
     }
