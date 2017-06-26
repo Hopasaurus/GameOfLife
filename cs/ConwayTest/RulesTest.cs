8122a2d58e00db2155485dd7fdeb4ba07bdd5fe8
David Hoppe <dave@hopasaurus.com>
2017-06-25 21:43:43 -0400

    Added a setup function for use in testing the rules class.
    Discovered that it is looking like mock may be the way to go.
    Still exploring non-mocking to carry out the thought experiment and
    see if the cell class implementation will be changed and cause this test to
    explode.
    
    If time permits I may pretend that there is a business reason to change the
    cell class in a way that will break this.  Mostly to see how far reaching
    the breakage is.

--- cs/ConwayTest/RulesTest.cs
+++ cs/ConwayTest/RulesTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Conway;
+﻿using Conway;
 using NUnit.Framework;
 
 namespace ConwayTest
@@ -28,8 +23,62 @@ namespace ConwayTest
             // If counting neighbors on the cell were to cause disk/network access or
             // were computationally intense the mocks would be more likely to be used.
 
-            var cell = new Cell();
-            Rules.Foo();
+            var cell = SetupCell(CellStatus.Living, 0);
+
+            Assert.AreEqual(0, cell.CountLivingNeighbors());
+
+            // quick check to make sure the setup function is sane.
+            for (var i = 0; i <= 8; ++i)
+            {
+                Assert.AreEqual(i, SetupCell(CellStatus.Living, i).CountLivingNeighbors());
+            }
+            
+            //Rules.Foo();
         }
+
+        private static Cell SetupCell(CellStatus status, int livingNeighbors)
+        {
+
+            var cell = new Cell(status);
+
+            var cellStatuses = new CellStatus[8];
+
+            // Will think about making this prettier later. Would normally pretty it up before committing
+            // it is interesting to capture this in this case.
+            // A little hackish, but should work fine.  (Again, starting to look like an argument towards mocking.)
+            for (var i = 0; i < 8; ++i)
+            {
+                cellStatuses[i] = i < livingNeighbors ? CellStatus.Living : CellStatus.Dead;
+            }
+            
+            // Add cardinal neighbors
+            var west = new Cell(cellStatuses[0]);
+            cell.AddNeighbor(west, Direction.West);
+
+            var east = new Cell(cellStatuses[1]);
+            cell.AddNeighbor(east, Direction.East);
+            
+            cell.AddNeighbor(new Cell(cellStatuses[2]), Direction.North);
+            cell.AddNeighbor(new Cell(cellStatuses[3]), Direction.South);
+
+            // Add inter-cardinal neighbors - note we are 'cheating' a little by knowing the cell implementation
+            // does not need full linkage. This could come back to haunt us if cell counting changes.
+            // That is not good as this is a test for the rules and not the cell.
+            //   - this seems to be arguing that we should do better setup or use mocking to isolate this test
+            //      from changes to the cell class.
+            // Leaving the 'cheat' in place as it was my first inclination and it might be interesting to 
+            //  see if it leads to any issues.  I suspect it will not as I suspect that the cell class will not
+            //  change but still it is interesting to become more aware of an think about.  How often does something
+            //  like this happen in production code?
+            
+            west.AddNeighbor(new Cell(cellStatuses[4]), Direction.North);
+            west.AddNeighbor(new Cell(cellStatuses[5]), Direction.South);
+
+            east.AddNeighbor(new Cell(cellStatuses[6]), Direction.North);
+            east.AddNeighbor(new Cell(cellStatuses[7]), Direction.South);
+
+            return cell;
+        }
+
     }
 }
