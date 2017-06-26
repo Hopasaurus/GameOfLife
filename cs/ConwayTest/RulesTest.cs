6896c4166150ac5b7234534912e5f844e33eca47
David Hoppe <dave@hopasaurus.com>
2017-06-25 22:33:26 -0400

    Clean up rules tests.
    Removed comments that are no longer needed.
    Decided to leave the setup as is as.  It is test code which is not an excuse
    to intentionally make it dirty.  At the moment I do not see a way to clean
    it up.  Likely I would make a note of this and check it the next day to
    see if any bright ideas pop into my head.
    
    I think part of the reason I am stuck on it is that I am anxious to get
    to the grid code and get something on the screen.
    
    Stay tuned, I think we are about to discover a limitation on the cell class,
    there is not currently a provision to update it without botching up the status
    for neighbors to look at while updating the grid.
    I suspect we will end up with a flag on the cell indicating it's next status
    and also suspect we will need to scan the grid to check for updates, then
    run the grid again to effect the update. This implementation is turning out
    to be a bit heavy. (lots of rather bigish cell objects and running the grid
    twice per update)  Not worried about that though, this is the first go round
    and I think it will be more than fast enough to keep the business people
    happy while we figure out how to rework things.  Also it may be good forever
    I do not recall having seen any perf spec.

--- cs/ConwayTest/RulesTest.cs
+++ cs/ConwayTest/RulesTest.cs
@@ -6,34 +6,6 @@ namespace ConwayTest
     [TestFixture]
     public class RulesTest
     {
-        // pausing to think here....
-        // should we use mocking to answer the neighbor count?
-        // that seems heavy handed. I think for this case
-        // make a setup method that we can give a neighbor count to
-        // and have it set the cell up with the correct number of living neighbors.
-        // If counting neighbors on the cell were to cause disk/network access or
-        // were computationally intense the mocks would be more likely to be used.
-
-
-        
-        // Rules are:
-        // x Live cell with less than two neighbors dies
-        // x Live cell with two or three neighbors lives
-        // x Live cell with more than three neighbors dies
-        // _ Dead cell with three neighbors comes to life
-        // *** implied rule Dead cell with other than three neighbors stays dead
-
-        [TestCase(0)]
-        [TestCase(1)]
-        public void Test_LiveCellWithLessThanTwoNeighbors_Dies(int neighbors)
-        {
-            var cell = SetupCell(CellStatus.Living, neighbors);
-
-            var result = Rules.NextStatus(cell);
-
-            Assert.AreEqual(CellStatus.Dead, result);
-        }
-
         [TestCase(2)]
         [TestCase(3)]
         public void Test_LiveCellWithTwoOrThreeNeighbors_Lives(int neighbors)
@@ -45,12 +17,14 @@ namespace ConwayTest
             Assert.AreEqual(CellStatus.Living, result);
         }
 
+        [TestCase(0)]
+        [TestCase(1)]
         [TestCase(4)]
         [TestCase(5)]
         [TestCase(6)]
         [TestCase(7)]
         [TestCase(8)]
-        public void Test_LiveCellWithMoreThanThreeNeighbors_Dies(int neighbors)
+        public void Test_LiveCellWithLessThanTwoOrMoreThanThreeNeighbors_Dies(int neighbors)
         {
             var cell = SetupCell(CellStatus.Living, neighbors);
 
@@ -86,51 +60,17 @@ namespace ConwayTest
             Assert.AreEqual(CellStatus.Living, result);
         }
 
-        [TestCase(CellStatus.Living, 0, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 1, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 2, CellStatus.Living)]
-        [TestCase(CellStatus.Living, 3, CellStatus.Living)]
-        [TestCase(CellStatus.Living, 4, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 5, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 6, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 7, CellStatus.Dead)]
-        [TestCase(CellStatus.Living, 8, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 0, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 1, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 2, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 3, CellStatus.Living)]
-        [TestCase(CellStatus.Dead, 4, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 5, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 6, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 7, CellStatus.Dead)]
-        [TestCase(CellStatus.Dead, 8, CellStatus.Dead)]
-        public void Test_AllTheThingsInOneMassiveTest(CellStatus initialStatus, int livingNeighbors,
-            CellStatus expectedStatus)
-        {
-            var cell = SetupCell(initialStatus, livingNeighbors);
-
-            var result = Rules.NextStatus(cell);
-
-            Assert.AreEqual(expectedStatus, result);
-        }
-
-
         private static Cell SetupCell(CellStatus status, int livingNeighbors)
         {
-
             var cell = new Cell(status);
 
             var cellStatuses = new CellStatus[8];
 
-            // Will think about making this prettier later. Would normally pretty it up before committing
-            // it is interesting to capture this in this case.
-            // A little hackish, but should work fine.  (Again, starting to look like an argument towards mocking.)
             for (var i = 0; i < 8; ++i)
             {
                 cellStatuses[i] = i < livingNeighbors ? CellStatus.Living : CellStatus.Dead;
             }
             
-            // Add cardinal neighbors
             var west = new Cell(cellStatuses[0]);
             cell.AddNeighbor(west, Direction.West);
 
@@ -140,16 +80,6 @@ namespace ConwayTest
             cell.AddNeighbor(new Cell(cellStatuses[2]), Direction.North);
             cell.AddNeighbor(new Cell(cellStatuses[3]), Direction.South);
 
-            // Add inter-cardinal neighbors - note we are 'cheating' a little by knowing the cell implementation
-            // does not need full linkage. This could come back to haunt us if cell counting changes.
-            // That is not good as this is a test for the rules and not the cell.
-            //   - this seems to be arguing that we should do better setup or use mocking to isolate this test
-            //      from changes to the cell class.
-            // Leaving the 'cheat' in place as it was my first inclination and it might be interesting to 
-            //  see if it leads to any issues.  I suspect it will not as I suspect that the cell class will not
-            //  change but still it is interesting to become more aware of an think about.  How often does something
-            //  like this happen in production code?
-            
             west.AddNeighbor(new Cell(cellStatuses[4]), Direction.North);
             west.AddNeighbor(new Cell(cellStatuses[5]), Direction.South);
 
